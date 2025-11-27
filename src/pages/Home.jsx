e0f6f9ee520b2ca377c7e1257c0cbb526d63906e
busybert <bertyz@gmail.com>
2025-11-27 02:06:50 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react";
-
-// Import your section components
 import Hero from "../components/Hero.jsx";
 import Features from "../components/Features.jsx";
 import Pricing from "../components/Pricing.jsx";
-import Services from "../components/Services.jsx";
-import Map from "../components/Map.jsx";
 import Footer from "../components/Footer.jsx";
 
 const Home = () => {
@@ -13,13 +9,10 @@ const Home = () => {
     <>
       <Hero />
       <Features />
-      <Services />
       <Pricing />
-      <Map />
       <Footer />
     </>
   );
 };
 
 export default Home;
-
