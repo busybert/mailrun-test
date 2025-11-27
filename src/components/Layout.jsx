d20ff85c3d400a5988d5f50d4aa2b055f5f33270
busybert <bertyz@gmail.com>
2025-11-27 00:48:52 -0500

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
+import React from "react";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-900 text-white">
-      <Header />
-      <main className="flex-grow pt-20">{children}</main>
+    <div className="min-h-screen flex flex-col bg-slate-950 text-white">
+      <Navbar />
+
+      <main className="flex-grow">
+        {children}
+      </main>
+
       <Footer />
     </div>
   );
