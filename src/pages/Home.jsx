a761621b828925a6b1d607b11a9f63ba52a73235
busybert <bertyz@gmail.com>
2025-11-27 10:18:19 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,9 @@
 import Hero from "@/components/ui/Hero.jsx";
-import Pricing from "@/components/ui/Pricing.jsx";
-import Features from "@/components/ui/Features.jsx";
 
 const Home = () => {
   return (
     <>
       <Hero />
-
-      {/* Add your other homepage components here */}
     </>
   );
 };
