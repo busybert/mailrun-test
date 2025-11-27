d028bdb71628b9229bea7243c4250369c1a32826
busybert <bertyz@gmail.com>
2025-11-27 11:05:31 -0500

    Update toaster.jsx

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -1,34 +1,13 @@
-import React from 'react';
-import {
-	Toast,
-	ToastClose,
-	ToastDescription,
-	ToastProvider,
-	ToastTitle,
-	ToastViewport,
-} from '@/components/ui/toast';
-import { useToast } from '@/components/ui/use-toast';
+import * as React from "react"
+import { Toast, ToastProvider } from "./toast.jsx"   // FIXED: removed "@/"
 
-export function Toaster() {
-	const { toasts } = useToast();
-
-	return (
-		<ToastProvider>
-			{toasts.map(({ id, title, description, action, ...props }) => {
-				return (
-					<Toast key={id} {...props}>
-						<div className="grid gap-1">
-							{title && <ToastTitle>{title}</ToastTitle>}
-							{description && (
-								<ToastDescription>{description}</ToastDescription>
-							)}
-						</div>
-						{action}
-						<ToastClose />
-					</Toast>
-				);
-			})}
-			<ToastViewport />
-		</ToastProvider>
-	);
+export function Toaster({ children }) {
+  return (
+    <ToastProvider>
+      {children}
+      <Toast />
+    </ToastProvider>
+  )
 }
+
+export default Toaster;
