e19f6b69e33d78fcc88fd95e0df6fcaec263ca9c
busybert <bertyz@gmail.com>
2025-11-27 10:33:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
+  // IMPORTANT: no path aliases!
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: {}
   },
+
   server: {
     port: 3000,
   },
+
   build: {
     outDir: "dist",
   },
