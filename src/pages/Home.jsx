d5386a5c127d42ae58214cf287319cd8e08fa8af
busybert <bertyz@gmail.com>
2025-11-27 01:54:31 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,1 +1,25 @@
+import React from "react";
+
+// Import your section components
+import Hero from "../components/Hero.jsx";
+import Features from "../components/Features.jsx";
+import Pricing from "../components/Pricing.jsx";
+import Services from "../components/Services.jsx";
+import Map from "../components/Map.jsx";
+import Footer from "../components/Footer.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <Features />
+      <Services />
+      <Pricing />
+      <Map />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
 
